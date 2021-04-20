b03cfe0705c7e0471783277d1f4bf1516c17c4e9
victorb132 <victornovais@MacBook-Pro-de-Victor.local>
2021-04-20 19:39:51 -0300

    Navigation routes

---
+++ src/screens/Welcome.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Image,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
+import { Feather } from '@expo/vector-icons'
+
+import wateringImg from '../assets/watering.png';
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+import { useNavigation } from '@react-navigation/native';
+
+export function Welcome() {
+  const navigation = useNavigation();
+
+  const handleStart = () => {
+    navigation.navigate('UserIdentification');
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>
+        Gerencie {'\n'}
+        suas plantas de {'\n'}
+        forma fácil
+      </Text>
+
+      <Image 
+        source={wateringImg}
+        style={styles.image}
+        resizeMode="contain"
+      />
+
+      <Text style={styles.subtitle}>
+        Não esqueça mais de regar suas {`\n`} plantas.
+        Nós cuidamos de lembrar você {`\n`} sempre que precisar.
+      </Text>
+
+      <TouchableOpacity
+      style={styles.button}
+      activeOpacity={0.7}
+      onPress={handleStart}
+    >
+      <Feather name="chevron-right" style={styles.iconButton} />
+    </TouchableOpacity>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: '600',
+    textAlign: 'center',
+    color: colors.heading,
+    marginTop: 38,
+    fontFamily: fonts.heading,
+    lineHeight: 38
+  },
+  subtitle: {
+    textAlign: 'center',
+    fontSize: 17,
+    paddingHorizontal: 20,
+    color: colors.heading,
+    fontFamily: fonts.text,
+  },
+  image: {
+    height: Dimensions.get('window').width * 0.7,
+  },
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginRight: 10,
+    height: 56,
+    width: 56
+  },
+  iconButton: {
+    fontSize: 32,
+    color: colors.white,
+  },
+})
